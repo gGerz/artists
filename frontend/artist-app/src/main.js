b509a980f862c0ea5761d5dffc7d588614c51a2b
german <german@smartworld.team>
2020-05-05 12:15:58 +0700

    base structure

---
+++ frontend/artist-app/src/main.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import App from './App.vue'
+import { domain } from './globalVariables'
+import router from './router'
+import store from './store'
+import axios from 'axios'
+
+Vue.config.productionTip = false
+
+// Axios options
+// Vue.options.root = domain
+// Vue.prototype.$http = axios
+// axios.defaults.withCredentials = true
+//
+// Vue.use(axios)
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
